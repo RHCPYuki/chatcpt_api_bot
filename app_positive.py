35690979cfcf0aa4dfc7f6f2b522a1acc8ad2ae7
RHCPYuki <132656137+RHCPYuki@users.noreply.github.com>
2023-05-08 13:21:30 +0900

    Add files via upload

---
+++ app_positive.py
@@ -0,0 +1,55 @@
+# 以下を「app.py」に書き込み
+import streamlit as st
+import openai
+import secret_keys  # 外部ファイルにAPI keyを保存
+
+openai.api_key = secret_keys.openai_api_key
+
+system_prompt = """
+あなたはポジティブな相談相手です。
+どんなことでも、相手に合わせて常にポジティブになれるように励ましてください。
+あなたの役割は相手を励ますことなので、悲観的なアドバイスはしないでください。
+
+
+"""
+
+# st.session_stateを使いメッセージのやりとりを保存
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "system", "content": system_prompt}
+        ]
+
+# チャットボットとやりとりする関数
+def communicate():
+    messages = st.session_state["messages"]
+
+    user_message = {"role": "user", "content": st.session_state["user_input"]}
+    messages.append(user_message)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )  
+
+    bot_message = response["choices"][0]["message"]
+    messages.append(bot_message)
+
+    st.session_state["user_input"] = ""  # 入力欄を消去
+
+
+# ユーザーインターフェイスの構築
+st.title(" 「ポジティブ」ボット")
+st.image("04_programming.png")
+st.write("何でも相談してください。")
+
+user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+    for message in reversed(messages[1:]):  # 直近のメッセージを上に
+        speaker = "🙂"
+        if message["role"]=="assistant":
+            speaker="🤖"
+
+        st.write(speaker + ": " + message["content"])
